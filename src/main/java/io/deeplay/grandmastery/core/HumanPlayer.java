65e844f278f6d77820d53896f8e7a3c3d8ee45e0
motomoto8913 <137675259+motomoto8913@users.noreply.github.com>
2023-08-05 15:30:30 +0700

    fix codestyle

--- src/main/java/io/deeplay/grandmastery/core/HumanPlayer.java
+++ src/main/java/io/deeplay/grandmastery/core/HumanPlayer.java
@@ -2,7 +2,6 @@ package io.deeplay.grandmastery.core;
 
 import io.deeplay.grandmastery.domain.Color;
 import io.deeplay.grandmastery.exceptions.GameException;
-
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
