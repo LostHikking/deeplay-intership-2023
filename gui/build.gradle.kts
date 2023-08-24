77d5c387d68ca1381cfa70e723ae9000a0cadca4
motomoto8913 <matvey.yurkevich@gmail.com>
2023-08-24 17:07:17 +0700

    fix: убрал junit 4

--- gui/build.gradle.kts
+++ gui/build.gradle.kts
@@ -3,5 +3,4 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("org.assertj:assertj-swing:3.9.2")
     testImplementation("org.assertj:assertj-swing-junit:3.9.2")
-    testImplementation("junit:junit:4.13.1")
 }
