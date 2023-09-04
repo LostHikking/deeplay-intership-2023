6c16129221a71a047c747df07ec87388b2e83f57
Dmitry Stacenko <dimastacenko104@gmail.com>
2023-09-04 11:36:10 +0700

    feat(LjeDmitry): Первая версия minmax

---
+++ ljedmitry/src/main/java/io/deeplay/grandmastery/AiBotsGame.java
@@ -0,0 +1,49 @@
+package io.deeplay.grandmastery;
+
+import io.deeplay.grandmastery.core.AiPlayer;
+import io.deeplay.grandmastery.core.GameController;
+import io.deeplay.grandmastery.domain.ChessType;
+import io.deeplay.grandmastery.domain.Color;
+import io.deeplay.grandmastery.domain.GameState;
+import lombok.extern.slf4j.Slf4j;
+
+/** Главный класс, который запускает локальную игру в шахматы. */
+@Slf4j
+public class AiBotsGame {
+  private static final int COUNT_TESTS = 10;
+  private static final boolean WITH_GUI = true;
+  private static Gui gui;
+
+  /**
+   * Локальная игра в шахматы.
+   *
+   * @param args Аргументы командной строки (не используются).
+   */
+  public static void main(String[] args) {
+    if (WITH_GUI) {
+      gui = new Gui(true);
+    }
+
+    for (int i = 1; i <= COUNT_TESTS; i++) {
+      try {
+        GameController gameController =
+            new GameController(new LjeDmitryBot(Color.WHITE), new AiPlayer(Color.BLACK));
+        gameController.beginPlay(ChessType.CLASSIC);
+        if (WITH_GUI) {
+          gui.showBoard(gameController.getBoard(), Color.WHITE);
+        }
+        while (!gameController.isGameOver()) {
+          gameController.nextMove();
+          if (WITH_GUI) {
+            gui.showBoard(gameController.getBoard(), Color.WHITE);
+          }
+        }
+
+        GameState status = gameController.getGameStatus();
+        log.info("Complete: " + i + ", result: " + status);
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+      }
+    }
+  }
+}
---
+++ ljedmitry/src/main/java/io/deeplay/grandmastery/LjeDmitryBot.java
@@ -0,0 +1,31 @@
+package io.deeplay.grandmastery;
+
+import io.deeplay.grandmastery.core.Move;
+import io.deeplay.grandmastery.core.Player;
+import io.deeplay.grandmastery.domain.Color;
+import io.deeplay.grandmastery.exceptions.GameException;
+
+public class LjeDmitryBot extends Player {
+  private final MiniMaxBot bot = new MiniMaxBot(this, 3);
+
+  /**
+   * Конструктор с параметрами.
+   *
+   * @param color Цвет
+   */
+  public LjeDmitryBot(Color color) {
+    super("LjeDmitry", color);
+  }
+
+  @Override
+  public Move createMove() throws GameException {
+    Move move = bot.findBestMove(game.getCopyBoard(), gameHistory);
+    lastMove = move;
+    return move;
+  }
+
+  @Override
+  public boolean answerDraw() throws GameException {
+    return false;
+  }
+}
